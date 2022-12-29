1a1abbbb092f6f8ee5849396eb67050a4ed66258
CornWorld <cw@had.ink>
2022-12-29 08:24:08 +0800

    [Feat] Remove the single border style of Toc

--- src/component/Toc/index.tsx
+++ src/component/Toc/index.tsx
@@ -4,27 +4,21 @@ import Typography from '@mui/material/Typography';
 
 const TocContainer = styled(Box)(({theme}) => ({
     marginTop: '2rem',
-    'ul': {
+    ul: {
         padding: 0,
         margin: 0,
         listStyle: 'none',
-        listStyleImage: 'initial',
-        'ul': {
-            marginBlockStart: 0,
-            marginBlockEnd: 0,
-            '>li a': {
-                paddingLeft: '+10px'
-            }
-        },
     },
-    'li': {
-        borderLeft: '1.2px solid transparent',
-        boxSizing: 'border-box',
-        '&:hover': {
-            borderLeftColor: 'rgb(120,120,120)',
-            color: 'rgb(208, 192, 167)',
+    li: {
+        li: {
+            paddingLeft: '+10px',
         },
-    }
+        transition: '0.1s',
+        color: '#000000',
+        'a:hover': {
+            color: '#575757',
+        }
+    },
 }))
 
 const TocTypography = styled(Typography)(({theme}) => ({
@@ -32,7 +26,7 @@ const TocTypography = styled(Typography)(({theme}) => ({
     borderLeftColor: 'transparent',
     textDecoration: 'none',
     alignItems: 'center',
-    fontSize: '0.9rem',
+    fontSize: '0.85rem',
     padding: '0px 8px 0px 10px',
     margin: '4px 0px 8px',
     fontWeight: 500,
@@ -49,7 +43,7 @@ export default function DreamCatToc() {
                 <li>
                     <TocTypography href={'#qwqSub'} component={'a'}>qwq</TocTypography>
                     <ul>
-                        <li><TocTypography href={'#qaq'} component={'a'}>qaq</TocTypography></li>
+                        <li><TocTypography href={'#qaq'} component={'a'}>qmq</TocTypography></li>
                         <li><TocTypography href={'#qmq'} component={'a'}>qmq</TocTypography></li>
 
                     </ul>
